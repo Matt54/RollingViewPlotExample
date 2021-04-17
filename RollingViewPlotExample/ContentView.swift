83a04c2bbf53c60b5f86f356f67107f09608cd3c
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2021-04-17 15:01:06 -0500

    Initial Commit

---
+++ RollingViewPlotExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RollingViewPlotExample
+//
+//  Created by Matt Pfeiffer on 4/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RollingViewPlotExample/RollingViewPlotExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingViewPlotExampleApp.swift
+//  RollingViewPlotExample
+//
+//  Created by Matt Pfeiffer on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct RollingViewPlotExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
